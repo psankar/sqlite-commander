fb313965d37d8dd5a00ac27708b71fd087aacba9
Sankar P <sankar.curiosity@gmail.com>
2010-12-21 21:25:58 +0530

    Get sqlite database filename from user and find tables in that.

--- Autobahn.cs
+++ Autobahn.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Data;
 using Mono.Terminal;
+using Mono.Data.SqliteClient;
 
 namespace Autobahn {
 
@@ -7,18 +9,42 @@ namespace Autobahn {
 
 		public Shell () : base (0, 0, Application.Cols, Application.Lines)
 		{
-			Add(new Label (0, 0, "Hello World"));
+			Add (new Label (0, 0, "Hello World"));
 		}
 
 		static void Main (String [] args) {
 
-			Console.WriteLine ("Hello World");
+			if (args.Length != 1)
+				Console.WriteLine ("Insufficient number of parameters");
+			else {
+				string connectionString;
+				string sql;
+				IDbCommand command;
+				IDbConnection db_connection;
+				IDataReader reader;
 
-			Application.Init (false);
+				connectionString = "URI=file:" + args[0];
+				db_connection = (IDbConnection) new SqliteConnection(connectionString);
+				db_connection.Open ();
+				command = db_connection.CreateCommand ();
+				sql = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name";
+				command.CommandText = sql;
+				reader = command.ExecuteReader ();
+				while (reader.Read ()) {
+					string TableName = reader.GetString (0);
+					Console.WriteLine("Name: " + TableName);
+				}
 
-			Shell s = new Shell ();
-			Application.Run (s);
+				// clean up
+				reader.Close (); reader = null;
+				command.Dispose (); command = null;
+				db_connection.Close (); db_connection = null;
 
+				Application.Init (false);
+
+				Shell s = new Shell ();
+				Application.Run (s);
+			}
 		}
 	}
 }
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SOURCES = Autobahn.cs
 
 Autobahn.exe: $(SOURCES) Makefile 
-	gmcs -debug $(SOURCES) -pkg:mono-curses
+	gmcs -debug $(SOURCES) -pkg:mono-curses -r:System.Data.dll -r:Mono.Data.SqliteClient.dll
 
 run: Autobahn.exe
-	mono --debug Autobahn.exe; stty sane
+	mono --debug Autobahn.exe urlclassifier3.sqlite; stty sane
